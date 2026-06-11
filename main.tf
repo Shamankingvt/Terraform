01605e98c57b14af21ec02dce44042b17f991148
Shamankingvt <thanhtuanvt93@gmail.com>
2026-06-11 17:39:30 +0700

    Added output value of s3_bucket configuration

--- main.tf
+++ main.tf
@@ -1,195 +1,66 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.46.0"
-    }
-  }
-}
-
 provider "aws" {
-  # Configuration options
-  region = "ap-southeast-1"
+    region = "us-east-2"
 }
 
-/* resource "aws_instance" "my_first_server" {
-  ami           = "ami-0576ef8e344fbf536"
-  instance_type = "t4g.nano"
-  vpc_security_group_ids = [ aws_security_group.instance.id ]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF 
-
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "HelloWorld"
-  }
-} */
-
-resource "aws_launch_configuration" "example" {
-  image_id = "ami-0576ef8e344fbf536"
-  instance_type = "t2.micro"
-  security_groups = [ aws_security_group.instance.id ]
-
-  user_data = <<-EOF
-  #!/bin/bash
-  echo "Hello, World" > index.html
-  nohup busybox httpd -f -p ${var.server_port} &
-  EOF
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = [ data.aws_subnets.default.id ]
-
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
-  
-  min_size = 1
-  max_size = 3
-
-  tag {
-  key = "Name"
-  value = "terraform-asg-example"
-  propagate_at_launch = true
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name = "vpc-id"
-    values = [ data.aws_vpc.default.id ]
-  }
-}
-
-resource "aws_lb" "example" {
-  name = "terraform-asg-example"
-  load_balancer_type = "application"
-  subnets = data.aws_subnets.default.ids
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port = 80
-  protocol = "HTTP"
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-    content_type = "text/plain"
-    message_body = "404: page not found"
-    status_code = 404
+terraform {
+    backend "s3" {
+        # Replace this with your bucket name!
+        bucket = "terraform-up-and-running-state"
+        key = "global/s3/terraform.tfstate"
+        region = "us-east-2"
+        # Replace this with your DynamoDB table name!
+        dynamodb_table = "terraform-up-and-running-locks"
+        encrypt = true
     }
-  }
 }
 
-resource "aws_lb_target_group" "asg" {
-  name = "terraform-asg-example"
-  port = var.server_port
-  protocol = "HTTP"
-  vpc_id = data.aws_vpc.default.id
-  health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200"
-    interval = 15
-    timeout = 3
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority = 100
-
-  condition {
-  path_pattern {
-    values = ["*"]
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "terraform-up-and-running-state"
+    # Prevent accidental deletion of this S3 bucket
+    lifecycle {
+        prevent_destroy = true
     }
-  }
-
-  action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
 }
 
-output "alb_dns_name" {
-  value = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
+resource "aws_s3_bucket_versioning" "enabled" {
+    bucket = aws_s3_bucket.terraform_state.id
+    versioning_configuration {
+        status = "Enabled"
+    }
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-  ingress {
-  from_port = var.server_port
-  to_port = var.server_port
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+    bucket = aws_s3_bucket.terraform_state.id
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
 }
 
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type = number
-  default = 8080
+resource "aws_s3_bucket_public_access_block" "public_access" {
+    bucket = aws_s3_bucket.terraform_state.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
 }
 
-resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
-  # Allow inbound HTTP requests
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-}
-  # Allow all outbound requests
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "terraform-up-and-running-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
 }
 
-/* output "public_ip" {
-  description = "The public IP address of the web server"
-  value = aws_instance.my_first_server.public_dns
-} */
-
-resource "aws_vpc" "first-vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "dev-vpc"
-  }
+output "s3_bucket_arn" {
+    value = aws_s3_bucket.terraform_state.arn
+    description = "The ARN of the S3 bucket"
 }
-
-resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.first-vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "dev-subnet"
-  }
+output "dynamodb_table_name" {
+    value = aws_dynamodb_table.terraform_locks.name
+    description = "The name of the DynamoDB table"
 }
-
-# resource "<provider>_<resource_type>" "name" {
-#       config option
-#       key = "value"
-#       key2 = "another value"
-#}
